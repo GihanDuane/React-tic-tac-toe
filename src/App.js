9878b2db4b64c9bd56334caab8e703075eb6bdd2
Gihan Duane <duanegaid.102094@gmail.com>
2023-05-31 23:22:32 +0800

    updated the sqaure button by removing value prop

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ function Board() {
   return (
     <>
       <div className="App">
-      < div className="board-row">
-          <Square value="1"/>
-          <Square value="2"/>
-          <Square value="3"/>
+        <div className="board-row">
+          <Square/>
+          <Square/>
+          <Square/>
         </div>
         <div className="board-row">
-          <Square value="4"/>
-          <Square value="5"/>
-          <Square value="6"/>
+          <Square/>
+          <Square/>
+          <Square/>
         </div>
         <div className="board-row">
-          <Square value="7"/>
-          <Square value="8"/>
-          <Square value="9"/>
+          <Square/>
+          <Square/>
+          <Square/>
         </div>
       </div>
     </>
