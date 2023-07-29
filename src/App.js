0dcec2b91793532911fcc40924b0958256ef60ec
Gihan Duane <duanegaid.102094@gmail.com>
2023-07-29 22:38:40 +0800

    added calculate winner component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import Square from './components/Square';
+import calculateWinner from './components/calculateWinner';
 import './App.css';
 import { useState } from 'react';
 
 function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if ( calculateWinner (squares) || squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext)
   }
+
+  const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "winner: " +  winner;
+    } else {
+      status = "Next Player: " + (xIsNext ? "X" : "O");
+    }
+
   return (
     <>
       <div className="App">
+        <div className="status">{status}</div>
         <div className="board-row">
           <Square value={squares[0]} onsquareClick={() => handleClick(0)}/>
           <Square value={squares[1]} onsquareClick={() => handleClick(1)}/>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,6 @@
 // import { useState } from "react";
 
 function Square({value, onsquareClick}) {
-    // const [value, setvalue] = useState(null);
-
-    // function handleCLick () {
-    //     // console.log("clicked!")
-    //     setvalue("X");
-    // }
-
     return (
     <button className="square" onClick={onsquareClick}>{value}</button>
     )
